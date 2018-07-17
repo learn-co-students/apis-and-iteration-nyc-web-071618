53623b1cb80ec89ed9eefa90182c15f944551480
David <davidlago104@gmail.com>
2018-07-17 16:17:04 -0400

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,13 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to this character list"
 end
 
 def get_character_from_user
+  require 'rest-client'
+  require 'json'
+
   puts "please enter a character"
+  gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
