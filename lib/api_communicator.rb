2733b4ab2e147327459826d5134efce73e91008c
TylerNodell <Tyler.l.nodell@gmail.com>
2018-07-17 14:28:44 -0400

    working batch

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,12 +6,23 @@ require 'pry'
 #   character_hash = JSON.parse(all_characters)
 #   binding.pry
 
+def get_api_results
+  i = 1
+  fullresults = []
+  9.times do
+    url = 'https://swapi.co/api/people/?page=' + i.to_s
+    all_char = RestClient.get(url)
+    char_hash = JSON.parse(all_char)
+    data = char_hash["results"]
+    fullresults.concat(data)
+    i+=1
+  end
+  fullresults
+end
+
 def get_character_movies_from_api(character)
-  #make the web request
-  all_char = RestClient.get('http://www.swapi.co/api/people/')
-  char_hash = JSON.parse(all_char)
-  char_data = char_hash["results"].find { |data|
-    data["name"].downcase == character.downcase
+  char_data = get_api_results.find { |data|
+    data["name"].downcase.include?(character.downcase)
   }
   film_urls = char_data["films"]
   film_data = film_urls.collect { |data|
@@ -28,11 +39,12 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+
 end
 
 def parse_character_movies(films_hash)
   i = 1
-  films_hash.each do |movie, i|
+  films_hash.each do |movie|
     puts "#{i}. #{movie["title"]}"
     i+=1
     
@@ -51,4 +63,4 @@ end
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
 
-get_character_movies_from_api('Luke Skywalker')
+# get_character_movies_from_api('luke skywalker')
