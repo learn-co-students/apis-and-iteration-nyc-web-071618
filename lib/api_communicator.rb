fbfcc28844f2ebd69944571f8304aaa893e0a49f
github username <github email address>
2018-07-17 12:57:08 -0400

    not done yet

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,7 +6,7 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,16 +16,34 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+  character_hash["results"].find do |results|
+    if results["name"] == character
+      film_array = results["films"]
+      film_array.each do |film|
+
+        film_api = RestClient.get(film)
+        film_hash = JSON.parse(film_api)
+      end
+    end
+  end
+  puts film_hash
 end
+get_character_movies_from_api("Luke Skywalker")
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  film_array = []
+  films_hash.each do |film_info|
+  end
+  film_array
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
+
+#show_character_movies("Luke Skywalker")
 
 ## BONUS
 
