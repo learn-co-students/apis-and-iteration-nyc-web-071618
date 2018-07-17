90434ed87bfae69ffec13e9a84c3eb0d99a381e9
cmonkey03 <russellpbrown@gmail.com>
2018-07-17 16:15:16 -0400

    Star Wars Character-Film Program

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,32 +2,45 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
-  #make the web request
+def find_character_hash(character_name)
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  found_character = character_hash['results'].find do |character|
+    character['name'].downcase == character_name.downcase
+  end
+  found_character
 end
 
-def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+def get_character_movies_from_api(character_name)
+  found_character = find_character_hash(character_name)
+  character_films = found_character['films']
+  films_arr = character_films.map do |film_url|
+    film_info = RestClient.get(film_url)
+    film_hash = JSON.parse(film_info)
+    film_hash
+  end
+ films_arr
 end
 
-def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+def parse_film_crawl(film)
+  crawl_sentences_arr = film['opening_crawl'].split("\r\n")
+  crawl_sentences_arr.each do |sentence|
+    puts sentence
+    sleep(2)
+  end
 end
 
-## BONUS
 
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
+def parse_character_movies(films_arr)
+  films_arr.each do |film|
+    puts film['title'].upcase
+    system('say "beep"')
+    parse_film_crawl(film)
+    puts "🛸" * 15
+  end
+end
+
+def show_character_movies(character_name)
+  films_arr = get_character_movies_from_api(character_name)
+  parse_character_movies(films_arr)
+end
